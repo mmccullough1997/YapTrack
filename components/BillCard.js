5721eedb9c0099922e48a6fbae6e7d715cbe0486
Mitch McCullough <mitchmccullough@gmail.com>
2022-08-27 12:13:18 -0500

    currentDate

--- components/BillCard.js
+++ components/BillCard.js
@@ -1,23 +1,34 @@
 import * as React from 'react';
-import Card from 'react-bootstrap/Card';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import PropTypes from 'prop-types';
 
-function BillCard({ billObj }) {
+export default function BillCards({ billObj }) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{billObj.payee}</Card.Title>
+    <Card sx={{ maxWidth: 300 }} className="billCard">
+      <CardHeader
+        className="billCardHeader"
+        subheader={billObj.payee}
+      />
+      <CardContent>
         <hr />
-        <Card.Title>Amount Due: ${billObj.amount}</Card.Title>
-        <Card.Title>Due on: {billObj.dueDate}</Card.Title>
+        <Typography variant="body2" color="text.secondary">Amount Due: ${billObj.amount}</Typography>
+        <Typography variant="body2" color="text.secondary">Due on: {billObj.dueDate}</Typography>
         <hr />
-        <Card.Subtitle>Last Payment</Card.Subtitle>
-      </Card.Body>
+        <Typography variant="paragraph" color="text.secondary">Last payment made on: {billObj.dueDate}</Typography>
+      </CardContent>
+      <IconButton aria-label="settings">
+        <MoreVertIcon />
+      </IconButton>
     </Card>
   );
 }
 
-BillCard.propTypes = {
+BillCards.propTypes = {
   billObj: PropTypes.shape({
     payee: PropTypes.string,
     amount: PropTypes.number,
@@ -25,5 +36,3 @@ BillCard.propTypes = {
     billFirebaseKey: PropTypes.string,
   }).isRequired,
 };
-
-export default BillCard;
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,22 @@
 
 import { useEffect, useState } from 'react';
 import { getUserBills } from '../api/billData';
-// import BillCard from '../components/BillCard';
-import BillCards from '../components/BillCards';
+import BillCard from '../components/BillCard';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
   const { user } = useAuth();
-  const date = new Date().toLocaleString();
   const [overdueBills, setOverdueBills] = useState([]);
-  // const [currentDate, setCurrentDate] = useState('');
+  const [currentDate, setCurrentDate] = useState('');
+
+  const getCurrentDate = () => {
+    const date = new Date();
+    setCurrentDate(date);
+  };
 
   useEffect(() => {
+    getCurrentDate();
     getUserBills(user.uid).then((userBillsArray) => {
-      console.warn(userBillsArray);
       setOverdueBills(userBillsArray);
     });
   }, []);
@@ -24,14 +27,42 @@ function Home() {
     <div>
       <div className="header">
         <h5>My Dashboard</h5>
-        <p>{date}</p>
+        <p>{`${currentDate.toLocaleString('default', { weekday: 'long' })}, ${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`}</p>
       </div>
+
       <hr />
-      <div className="d-flex flex-wrap">
-        {overdueBills.map((overdueBill) => (
-          <BillCards key={overdueBill.billFirebaseKey} billObj={overdueBill} />
-        ))}
+
+      <div>
+        <h6>Overdue:</h6>
+        <div className="d-flex flex-wrap">
+          {overdueBills.map((overdueBill) => (
+            <BillCard key={overdueBill.billFirebaseKey} billObj={overdueBill} />
+          ))}
+        </div>
       </div>
+
+      <hr />
+
+      <div>
+        <h6>Due Soon:</h6>
+        <div className="d-flex flex-wrap">
+          {overdueBills.map((overdueBill) => (
+            <BillCard key={overdueBill.billFirebaseKey} billObj={overdueBill} />
+          ))}
+        </div>
+      </div>
+
+      <hr />
+
+      <div>
+        <h6>Paid:</h6>
+        <div className="d-flex flex-wrap">
+          {overdueBills.map((overdueBill) => (
+            <BillCard key={overdueBill.billFirebaseKey} billObj={overdueBill} />
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 }
